808b4f38efde82adc58a713d7833916f9c1f626d
Sang <sangnn2012@gmail.com>
2024-01-31 18:04:55 +0700

    feat: init project with svelte

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,6 @@
+<header>
+    <div>
+        <img src="/icons/ytlogo.svg" alt="yt-icon" />
+        <img src="/icons/bugerIcon.svg" alt="buger-icon" />
+    </div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import './styles.css'
+    import Header from '../lib/components/Header.svelte';
+</script>
+
+<!-- Header apply to allp page -->
+<Header />
+
+<!-- Content slot -->
+<slot/>
