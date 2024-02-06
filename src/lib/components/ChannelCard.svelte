2a6581b56f0d89b6da65737504df135c6b6840b5
Sang <sangnn2012@gmail.com>
2024-02-06 15:21:06 +0700

    feat: add ChannelCard

---
+++ src/lib/components/ChannelCard.svelte
@@ -0,0 +1,17 @@
+<a href="/" class="flex gap-4 items-center">
+    <div class="max-x-[360px] w-full flex items-center justify-center">
+        <img alt="avatar" src="/avatar.png" class="w-[136px] h-[136px] rounded-full"/>
+    </div>
+
+    <div>
+        <h1 class="capitalize font-medium text-lg mb-2">
+            channel Name
+        </h1>
+        <p class="text-gray-600 text-xs mb-1">@channel . subscriber</p>
+        <p class="text-gray-600 text-xs">Welcome to the channel, lets talk about stuffs</p>
+    </div>
+
+    <button class="bg-black text-white text-sm ml-auto font-medium rounded-full p-1">
+        Subscribe
+    </button>
+</a>
