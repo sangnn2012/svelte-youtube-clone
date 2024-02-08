b63854a06a27f82633bbd6bfe167dd7b386ebd02
Sang <sangnn2012@gmail.com>
2024-02-08 17:06:48 +0700

    feat: fix sideBar styling

--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -19,10 +19,16 @@
     ];
 </script>
 
-<div class="bg-white py-2 flex flex-col px-1 fixed top-[60px] h-screen">
+<div class="bg-white py-2 md:flex flex-col px-1 fixed top-[60px] h-screen hidden">
     {#each nav as { icon, title }, i}
-        <a href="/" class="flex items-center justify-center flex-col gap-1 py-4 hover:bg-gray-100 rounded-lg">
-            <img src={`icons/${icon}.svg`} alt={icon} />
+        <a 
+            href="/" 
+            class="flex items-center justify-center flex-col gap-1 py-4 hover:bg-gray-100 rounded-lg"
+        >
+            <img 
+                src={`icons/${icon}.svg`} 
+                alt={icon}
+            />
             <p>{title}</p>
         </a>
     {/each}
